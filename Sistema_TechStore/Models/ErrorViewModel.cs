545cdc69323fedc88f2ab3c0172e99fda4d9d584
eamguille <eamguille@gmail.com>
2024-10-15 15:35:59 -0600

    adding empty project to repo

---
+++ Sistema_TechStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_TechStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
